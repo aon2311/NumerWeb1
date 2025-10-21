773ec2b3cab1ce4417de29bc603ceb681f49c479
aon2311 <aononwara39@gmail.com>
2025-10-22 00:10:59 +0700

    Simpson

--- numer/src/NumerCode/Integra/Sim.jsx
+++ numer/src/NumerCode/Integra/Sim.jsx
@@ -1,141 +1,116 @@
 import Navbar from "../../Components/Narbar/Navbar"
 import { useState } from "react"
-import { evaluate } from "mathjs"
+import { evaluate } from "mathjs";
 import "./Sim.css"
 
 function Sim() {
-    const [fx, setFx] = useState("")
-    const [a, setA] = useState("")
-    const [b, setB] = useState("")
-    const [n, setN] = useState("")
-    const [tolerance, setTolerance] = useState("0.000001")
-    const [result, setResult] = useState([])
+    const [fx,setFx] = useState("x^2")
+    const [a,setA] = useState ("-2")
+    const [b,setB] = useState("4")
+    const [tolerance,setTolerance] = useState("0.000001")
+    const [result,setResult] = useState([]) 
+    
+    const f=(x)=>evaluate(fx,{x})
 
-    const f = (x) => evaluate(fx, { x })
+    const IntegrateFx=(a,b)=>{
+        const h=(b-a)/2
+        let sum = f(a) +(4*f(h))+ f(b)
+        return (h/3) *sum
+    }
 
-    const IntegrateFx = (f, a, b, n) => {
-        const h = (b - a) / n
-        let sum = f(a) + f(b)
-
-        for (let i = 1; i < n; i++) {
-            const x = a + i * h
-            if (i%2==0) {
-                sum=sum+(2*f(x))
-            }
-            else if(i%2==1){
-                sum=sum+(4*f(x))
-            }
-            
+    const Integrate=(a,b,step=1000)=>{
+        const h= (b-a)/step
+        let xx = f(a)+f(b)
+        for(let i = 1 ;i<step;i++){
+            const x0=a+(i*h)
+            const  x1=x0+h
+            const xh=(x1+x0)/2
+            xx+=4*f(xh)
         }
+        return h/3 * xx
 
-        return (h / 3) * sum;
-    };
-
-    const CalSim = () => {
-        let A = parseFloat(a)
-        let B = parseFloat(b)
-        let N = parseInt(n)
+    }
+    const CalSim =()=>{
+        let A =parseFloat(a)
+        let B =parseFloat(b)
         let Tol = parseFloat(tolerance)
 
-        if (
-            isNaN(A) || isNaN(B) || isNaN(N) ||
-            a === "" || b === "" || tolerance === "" || n === ""
-        ) {
-            alert("Please fill all information.")
+        if (isNaN(A)||isNaN(B)||isNaN(Tol||a==""||b==""||tolerance=="")) {
+            alert ("Please fill all information")
             return;
-        }
-
-        if (N % 2 !== 0) {
-           alert("N must be an even number.")
-           return;
         }
 
         let iteration = 0
         let error = 1
-        let I_old = 0
-        const logs = []
+        let I_tr = Integrate(A,B)
+        const logs=[]
 
-        while (error > Tol) {
-            const I_new = IntegrateFx(f, A, B, N)
-            error =Math.abs((I_new - I_old) / I_new)
-
-            logs.push({
-                iteration: iteration,
-                A: A.toFixed(6),
-                B: B.toFixed(6),
-                I_new: I_new.toFixed(6),
-                error:  error.toFixed(tolerance.length - 2)||"N/A",
-            })
-
+        if (error>iteration) {
+            let I=IntegrateFx(A,B)
+            error = Math.abs((I_tr-I)/I_tr)
             
-
-            I_old = I_new
-            iteration++
-            N=N+2
-            if (iteration > 1000){
-                break
-            } 
+            logs.push({
+                I:I.toFixed(6),
+                error:error.toFixed(tolerance.length-2)||"N/A",
+            })
+             
         }
-
-        setResult(logs);
-    };
+        setResult(logs)
+    }
 
     return (
         <>
-            <Navbar />
-            <div className="Sim"><h1>Simpson's Rule</h1></div>
+            <Navbar/>
+            <div className="Sim"><h1>Simpson Rule's</h1> </div>
             <div className="Container">
-                <div className="function-group">
-                    <label>Function f(x)</label>
-                    <input value={fx} onChange={(e) => setFx(e.target.value)} />
-                </div>
                 <div className="input-container">
-                    <div className="input-box">
-                        <label>N (even number)</label>
-                        <input value={n} onChange={(e) => setN(e.target.value)} />
+                    <div className="function-group">
+                        <label >Function f(x):</label>
+                        <input className="wide-input" value={fx} onChange={(e)=>setFx(e.target.value)} />
                     </div>
-                    <div className="input-box">
-                        <label>a</label>
-                        <input value={a} onChange={(e) => setA(e.target.value)} />
+                    <div className="parameter-group">
+                        <div className="input-box">
+                            <label >a</label>
+                            <input value={a} onChange={(e)=>setA(e.target.value)} />
+                        </div>
+                        <div className="input-box">
+                            <label >b</label>
+                            <input value={b} onChange={(e)=>setB(e.target.value)} />
+                        </div>
+                        <div className="input-box">
+                            <label >Tolerance</label>
+                            <input value={tolerance} onChange={(e)=>setTolerance(e.target.value)} />
+                        </div>
                     </div>
-                    <div className="input-box">
-                        <label>b</label>
-                        <input value={b} onChange={(e) => setB(e.target.value)} />
-                    </div>
-                    <div className="input-box">
-                        <label>Tolerance</label>
-                        <input
-                            value={tolerance}
-                            onChange={(e) => setTolerance(e.target.value)}
-                            placeholder="0.000001"
-                        />
-                    </div>
+
                 </div>
+
             </div>
 
             <button className="confirm" onClick={CalSim}>Confirm</button>
 
-            <div className="result">Result</div>
             <table>
                 <thead>
                     <tr>
-                        <th>Iteration</th>
-                        <th>Integral Value</th>
+                        <th>F(x)</th>
                         <th>Error</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {result.map((row, idx) => (
-                        <tr key={idx}>
-                            <td>{row.iteration}</td>
-                            <td>{row.I_new}</td>
+                    {result.map((row,idx)=>(
+                        <tr key ={idx}>
+                            <td>{row.I}</td>
                             <td>{row.error}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+
+
+
+
         </>
-    );
+    )
 }
 
-export default Sim;
+export default Sim
